02d5517b3d9433d7f5c89eecc648c0b041a55954
Fawzi ElZein <Fawzi_zein_94@hotmail.com>
2023-07-07 13:32:41 +0300

    adding hide toggle password functionality to Signup page

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,15 +21,24 @@
 
 
 const togglePassword = document
-            .querySelector('#togglePassword');
-        const password = document.querySelector('#password');
-        togglePassword.addEventListener('click', () => {
-            // Toggle the type attribute using
-            // getAttribure() method
-            const type = password
-                .getAttribute('type') === 'password' ?
-                'text' : 'password';
-            password.setAttribute('type', type);
-            // Toggle the eye and bi-eye icon
-            this.classList.toggle('bi-eye');
-        });
+    .querySelector('#togglePassword');
+const password = document.querySelector('#password');
+togglePassword.addEventListener('click', () => {
+    const type = password
+        .getAttribute('type') === 'password' ?
+        'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+});
+
+
+const toggleRetypedPassword = document
+    .querySelector('#toggleRetypedPassword');
+const retypedPassword = document.querySelector('#retypedpassword');
+toggleRetypedPassword.addEventListener('click', () => {
+    const type = retypedPassword
+        .getAttribute('type') === 'password' ?
+        'text' : 'password';
+    retypedPassword.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+});
